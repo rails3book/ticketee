5693b3edd21bd4efcb1270ddf43e4ab7f74870f3
Ryan Bigg <radarlistener@gmail.com>
2011-05-07 14:05:43 +1000

    Added a seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ <%= key_value :name, "'Chicago'" %> }, { <%= key_value :name, "'Copenhagen'" %> }])
-#   Mayor.create(<%= key_value :name, "'Daley'" %>, <%= key_value :city, "cities.first" %>)
+admin_user = User.create(:email => "admin@ticketee.com",
+                         :password => "password")
+admin_user.admin = true
+admin_user.confirm!
+
+Project.create(:name => "Ticketee Beta")
