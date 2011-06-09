a9cdde9c079da55a64c3979314b310a9d33a1d5d
Ryan Bigg <radarlistener@gmail.com>
2011-06-09 15:09:47 +1000

    Ticket notifications are now a background job

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { Factory(:user) }
+  
+  before do
+    @ticket = Factory(:ticket)
+    @ticket.watchers << user
+  end
+
+  it "notifies people through a delayed job" do
+    Delayed::Job.count.should eql(0)
+    ticket.comments.create!(:text => "This is a comment",
+                            :user => ticket.user)
+    Delayed::Job.count.should eql(1)
+
+    Delayed::Worker.new.work_off!
+    Delayed::Job.count.should eql(0)
+
+    email = ActionMailer::Base.deliveries.last
+    email.to.should eql(user.email)
+  end
 end
