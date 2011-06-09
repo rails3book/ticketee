e2696b76792de24ddf2e5eba8dcfccf96234610a
Ryan Bigg <radarlistener@gmail.com>
2011-06-09 14:56:08 +1000

    Add ETag and Last-Modified                support to ticket show page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -32,6 +32,8 @@ class TicketsController < ApplicationController
   def show
     @comment = @ticket.comments.build
     @states = State.all
+    fresh_when :last_modified => @ticket.updated_at,
+               :etag => @ticket.to_s + current_user.id.to_s
   end
   
   def edit
