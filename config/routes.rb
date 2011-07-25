a5726cd4ca3ca6a407f4422c7a857dec15b6f1d3
Ryan Bigg <radarlistener@gmail.com>
2011-07-25 21:27:14 +1000

    Override Devise::RegistrationsController to work around sign up root_path asking for auth
    Conflicts:
    
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,14 @@ EdgeTicketee::Application.routes.draw do
       end
     end
   end
+
+  devise_for :users, :controllers => { 
+    :registrations => "registrations",
+  }
+  
+  get '/awaiting_confirmation', 
+    :to => "users#confirmation", 
+    :as => 'confirm_user'
   
   devise_for :users, :controllers => { 
     :omniauth_callbacks => "users/omniauth_callbacks" 
